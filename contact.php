a233424e347a540c66f6654c5a06bf58a9ceed41
randiayeshanikossinna <randiayeshani@gmail.com>
2020-11-13 07:16:12 -0800

    web project with connect database

---
+++ contact.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title>Contact Us</title>
+<link rel="stylesheet"  href="style.css">
+</head>
+<body>
+<div class="nav">
+         <ul>
+           <li><a class="active" href="#">Items</a></li>
+           <li><a href="#">Places</a></li>
+           <li><a href="#">Contact Us</a></li>
+         </ul> 
+     </div>
+     <div class="context">
+     <div class="form"> <br><br>
+      <label >First Name: <input type="text" name="fname"></label> <br><br>
+      <label >Last Name : <input type="text" name="lname"></label> <br><br>
+      <label >Email     : <input type="email" name="email"></label><br><br>
+      <label>Geneder  : <br>  Male <input type="radio" name="male" value="m"><br>
+                      female <input type="radio" name="female" value="f">
+      </label><br><br>
+      <label >Telephone : <input type="text" name="tele"></label><br><br>
+      <label >Country: 
+           <select >
+           <option selected hidden>Select Country</option>
+                <option value="coun1">Sri Lanka</option>
+                <option value="coun2">India</option>
+                <option value="coun3">USA</option>
+                <option value="coun4">Japan</option>
+                <option value="coun5">China</option>
+           </select>
+      </label><br><br>
+      <input type="submit">
+
+
+     </div>
+     </div>
+     <div class="last">
+         <h1>Take Away</h1>
+     </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>Pizza Hut</title>
+      <link rel="stylesheet"  href="style.css">
+      <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+      <div class="nav">
+         <ul>
+           <li><a class="active" href="#">Items</a></li>
+           <li><a href="#">Places</a></li>
+           <li><a href="#">Contact Us</a></li>
+         </ul> 
+     </div>
+     <div class="context" >
+         <div class="pic" >
+             <img src="is.jpg" alt="pizzapic">
+         </div>
+         <div class="text">
+             <p>Pizza Hut is an American restaurant chain and international franchise which was founded in 1958 in Wichita, Kansas by Dan and Frank Carney. The company is known for its Italian American cuisine menu, including pizza and pasta, as well as side dishes and desserts. Pizza Hut has 18,703 restaurants worldwide as of December 31, 2019, making it the world's largest pizza chain in terms of locations. It is a subsidiary of Yum! Brands, Inc., one of the world's largest restaurant companies.</p>
+             <div class="social">	
+ 			<ul>
+ 			<li><a href="#"><i class="fa fa-git" ></i></a></li>		
+ 			<li><a href="#"><i class="fa fa-linkedin"></i></a></li>	
+ 			<li><a href="#"><i class="fa fa-medium"></i></a></li>	
+ 			<li><a href="#"><i class="fa fa-facebook"></i></a></li>	
+            </ul>
+            </div>
+         </div>
+
+     </div>
+     <div class="last">
+         <h1>Take Away</h1>
+     </div>
+     
+    </body>
+</html>
